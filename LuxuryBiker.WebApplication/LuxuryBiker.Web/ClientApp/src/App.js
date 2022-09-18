7523d4f0b144cecfd92ca1ddf837161b858cc9bf
Cristian18-code <cristiangarzon180@gmail.com>
2022-09-17 19:22:34 -0500

    [Commit]:"Se agregan js y estilos"

--- LuxuryBiker.WebApplication/LuxuryBiker.Web/ClientApp/src/App.js
+++ LuxuryBiker.WebApplication/LuxuryBiker.Web/ClientApp/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout/Layout';
 
-import './styles/styles.min.css'
-
 export default class App extends Component {
   static displayName = App.name;
 
--- LuxuryBiker.WebApplication/LuxuryBiker.Web/ClientApp/src/index.js
+++ LuxuryBiker.WebApplication/LuxuryBiker.Web/ClientApp/src/index.js
@@ -1,4 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import './styles/styles.min.css';
+import './styles/vendor/css/all.min.css';
+import './vendor/jquery/jquery.min.js';
+import './vendor/bootstrap/js/bootstrap.bundle.min.js';
+import './vendor/jquery-easing/jquery.easing.min.js'
+import './js/LuxuryBiker.js'
+import './vendor/chart.js/Chart.min.js';
+import './js/demo/chart-area-demo.js';
+import './js/demo/chart-pie-demo.js';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
@@ -15,4 +25,3 @@ ReactDOM.render(
   rootElement);
 
 registerServiceWorker();
-
